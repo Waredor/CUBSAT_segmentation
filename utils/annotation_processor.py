890e1e0c61016e8c7ef674acc116af96a110ceea
Waredor <ivitaliyprosh.khabarovsk@gmail.com>
2025-07-29 15:51:24 +0300

    refactor(convert_labelme_to_yolo): add threadpoolexecutor to method

--- utils/annotation_processor.py
+++ utils/annotation_processor.py
@@ -7,6 +7,7 @@ import cv2
 import numpy as np
 
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor
 from PIL import Image
 
 class AnnotationProcessor:
@@ -15,12 +16,23 @@ class AnnotationProcessor:
     к тестовым изображениям.
     Parameters:
         class_names (list): список с именами используемых классов.
+        yolo_annotations_path (str): путь к директории с аннотациями в формате YOLO .txt
+        labelme_annotations_path (str): путь к директории с аннотациями в формате LabelMe .json
         logger (logging.Logger): объект логгера.
     """
 
-    def __init__(self, class_names: list, logger: logging.Logger) -> None:
+    def __init__(
+            self,
+            class_names: list,
+            yolo_annotations_path: str,
+            labelme_annotations_path: str,
+            logger: logging.Logger
+    ) -> None:
         self.class_names = class_names
         self.logger = logger
+        self.yolo_annotations_path = yolo_annotations_path
+        self.labelme_annotations_path = labelme_annotations_path
+        self.class_map = {name: idx for idx, name in enumerate(self.class_names)}
 
     def mask_to_polygons(self, mask: np.ndarray) -> list:
         """
@@ -58,9 +70,65 @@ class AnnotationProcessor:
 
         return polygons
 
+    def process_json_file(self, json_path: str) -> None:
+        """
+        Метод process_json_file() осуществляет конвертацию одного файла из
+        формата LabelMe .json в формат YOLO .txt
+        Parameters:
+            json_path (str): полный путь к .json файлу
+        Returns:
+            None
+        """
+        try:
+            with open(json_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+
+        except json.decoder.JSONDecodeError:
+            self.logger.warning(f"Empty file {f}")
+
+        image_filename = data.get('imagePath')
+        image_path = os.path.join(self.labelme_annotations_path, image_filename)
+
+        if not os.path.exists(image_path):
+            self.logger.warning(f"There are no images in: {image_path}")
+
+        with Image.open(image_path) as img:
+            w, h = img.size
+
+        yolo_lines = []
+        for shape in data['shapes']:
+            label = shape.get('label')
+            points = shape.get('points', [])
+
+            if label not in self.class_map:
+                self.logger.warning(f"Skipped class: '{label}' в {json_path}")
+                continue
+
+            if len(points) < 3:
+                self.logger.warning(f"Skipped object with a few points {json_path}")
+                continue
+
+            class_id = self.class_map[label]
+
+            norm_points = []
+            for x, y in points:
+                norm_x = round(x / w, 6)
+                norm_y = round(y / h, 6)
+                norm_points.extend([norm_x, norm_y])
+
+            yolo_line = f"{class_id} " + " ".join(map(str, norm_points))
+            yolo_lines.append(yolo_line)
+
+        base_name = os.path.splitext(os.path.basename(json_path))[0]
+        txt_path = os.path.join(self.yolo_annotations_path, base_name + ".txt")
+
+        with open(txt_path, 'w', encoding='utf-8') as f:
+            f.write("\n".join(yolo_lines))
+            self.logger.info(f"Converted {base_name}.txt")
+
     def create_labelme_json(
             self, image_path: str, masks: np.ndarray, labels: np.ndarray, output_dir: str
-    ) -> str:
+    ) -> None:
         """
         Метод create_labelme_json() осуществляет создание аннотаций к инференсу
         из InferenceRunner в формате .json
@@ -70,6 +138,8 @@ class AnnotationProcessor:
             labels (np.ndarray): numpy массив меток классов полигонов на изображении.
             output_dir (str): выходная директория для сохранения .json аннотаций
                 к изображениям.
+        Returns:
+            None
         """
         if not os.path.exists(output_dir):
             self.logger.error(f"Incorrect directory {output_dir}")
@@ -124,85 +194,35 @@ class AnnotationProcessor:
         with open(output_path, mode="w", encoding='utf-8') as f:
             json.dump(labelme_data, f, indent=2)
         self.logger.info(f"Created JSON-file: {output_path}")
-        return output_path
 
-    def convert_labelme_to_yolo(self, labelme_annotations_path: str,
-                                yolo_annotations_path: str) -> None:
+    def convert_labelme_to_yolo(self) -> None:
         """
         Метод convert_labelme_to_yolo() конвертирует аннотации изображений в формате .json LabelMe
         в формат YOLOv11 .txt
-        Parameters:
-            labelme_annotations_path (str): путь к директории с аннотациями в формате .json LabelMe.
-            yolo_annotations_path (str): путь к директории с аннотациями в формате .txt YOLOv11.
+        Returns:
+            None
         Raises:
             NotADirectoryError: если путь не является директорией
             FileNotFoundError: если файл по искомому пути не найден
         """
-        if not os.path.isdir(labelme_annotations_path):
-            self.logger.error(f"{labelme_annotations_path} is not a directory")
-            raise NotADirectoryError(f"{labelme_annotations_path} is not a directory")
+        if not os.path.isdir(self.labelme_annotations_path):
+            self.logger.error(f"{self.labelme_annotations_path} is not a directory")
+            raise NotADirectoryError(f"{self.labelme_annotations_path} is not a directory")
 
-        if not os.path.isdir(yolo_annotations_path):
-            self.logger.error(f"{yolo_annotations_path} is not a directory")
-            raise NotADirectoryError(f"{yolo_annotations_path} is not a directory")
+        if not os.path.isdir(self.yolo_annotations_path):
+            self.logger.error(f"{self.yolo_annotations_path} is not a directory")
+            raise NotADirectoryError(f"{self.yolo_annotations_path} is not a directory")
 
-        class_map = {name: idx for idx, name in enumerate(self.class_names)}
+        os.makedirs(self.yolo_annotations_path, exist_ok=True)
+        json_files = glob.glob(os.path.join(self.labelme_annotations_path, "*.json"))
 
-        os.makedirs(yolo_annotations_path, exist_ok=True)
-        json_files = glob.glob(os.path.join(labelme_annotations_path, "*.json"))
         if len(json_files) == 0:
-            self.logger.warning(f"There are no annotations in {labelme_annotations_path}")
-            raise FileNotFoundError(f"There are no annotations in {labelme_annotations_path}")
+            self.logger.warning(f"There are no annotations in {self.labelme_annotations_path}")
+            raise FileNotFoundError(f"There are no annotations in {self.labelme_annotations_path}")
 
         self.logger.info("Starting annotations convertation")
-        for json_path in json_files:
-            try:
-                with open(json_path, 'r', encoding='utf-8') as f:
-                    data = json.load(f)
 
-            except json.decoder.JSONDecodeError:
-                self.logger.warning(f"Empty file {f}")
-                continue
-
-            image_filename = data.get('imagePath')
-            image_path = os.path.join(labelme_annotations_path, image_filename)
-
-            if not os.path.exists(image_path):
-                self.logger.warning(f"There are no images in: {image_path}")
-                continue
-
-            with Image.open(image_path) as img:
-                w, h = img.size
-
-            yolo_lines = []
-            for shape in data['shapes']:
-                label = shape.get('label')
-                points = shape.get('points', [])
-
-                if label not in class_map:
-                    self.logger.warning(f"Skipped class: '{label}' в {json_path}")
-                    continue
-
-                if len(points) < 3:
-                    self.logger.warning(f"Skipped object with a few points {json_path}")
-                    continue
-
-                class_id = class_map[label]
-
-                norm_points = []
-                for x, y in points:
-                    norm_x = round(x / w, 6)
-                    norm_y = round(y / h, 6)
-                    norm_points.extend([norm_x, norm_y])
-
-                yolo_line = f"{class_id} " + " ".join(map(str, norm_points))
-                yolo_lines.append(yolo_line)
-
-            base_name = os.path.splitext(os.path.basename(json_path))[0]
-            txt_path = os.path.join(yolo_annotations_path, base_name + ".txt")
-
-            with open(txt_path, 'w', encoding='utf-8') as f:
-                f.write("\n".join(yolo_lines))
-                self.logger.info(f"Converted {base_name}.txt")
+        with ThreadPoolExecutor() as executor:
+            executor.map(self.process_json_file, json_files)
 
         self.logger.info("Annotations convertation is finished")
